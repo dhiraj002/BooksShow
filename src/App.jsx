26ea57280d889c152c946c51d3bcd11709bc7e92
Dhiraj Dhiman <dhirajdhiman15@gmail.com>
2025-03-15 12:54:10 +0530

    added keyframe and css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import Hero from "./components/Hero";
+// import "index.css";
 
 const App = () => {
+    const [searchQuery, setSearchQuery] = useState("Best Sellers");
     return (
         <>
             <div className="min-h-screen bg-gray-900">
-                <NavBar />
+                <NavBar hansleSearch={(term) => setSearchQuery(term)} />
                 <Banner />
-                <Hero />
+                <Hero searchQuery={searchQuery} />
             </div>
         </>
     );
